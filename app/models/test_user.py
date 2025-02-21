a881535af74e7cd4c6b318bad819ea3a33550d57
Nikita <shmerebek@gmail.com>
2025-02-22 01:21:53 +0500

    delete test.py

--- app/models/test_user.py
+++
@@ -1,7 +0,0 @@
-from ..extensions import db
-from sqlalchemy import Integer, String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-class UserTest(db.Model):
-    id : Mapped[int] = mapped_column(Integer(), primary_key=True)
-    title : Mapped[str] = mapped_column(String(50), nullable=False)
